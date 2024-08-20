16f8d18b36916d8013e27f42e91d41cefdcf01eb
serge <sergeykhlustkikov1@gmail.com>
2024-08-21 01:41:30 +0300

    MongoDB integration

---
+++ backend/db/init.js
@@ -0,0 +1,3 @@
+db = db.getSiblingDB('crydonkey');
+
+db.createCollection('app_settings');
--- backend/server.mjs
+++ backend/server.mjs
@@ -6,6 +6,7 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import rcadeRoutes from './routes/rcadeRoutes.mjs';
 import swanRoutes from './routes/swanRoutes.mjs';
+import records from './routes/records.mjs';
 
 const app = express();
 
@@ -15,11 +16,15 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
-app.use('/', rcadeRoutes)
-app.use('/', swanRoutes)
+app.use('/', rcadeRoutes);
+app.use('/', swanRoutes);
+app.use('/records', records);
 
-mongoose.connect(process.env.ATLAS_URI)
-  .then(db => console.log('[OK] DB is connected'))
+const dbConnectionURI = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+mongoose.connect(dbConnectionURI, {
+  useNewUrlParser: true,
+})
+  .then(db => console.log('[OK] DB is connected1'))
   .catch(err => console.error(err));
 
 app.get('/status', async (req, res) => {
